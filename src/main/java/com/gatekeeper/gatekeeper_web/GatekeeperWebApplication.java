cadc12690c2599506b2e90a5d4adc7ec0d4d5e1a
Pascal <paco@LAPTOP-LMBID7V2.localdomain>
2025-12-04 23:20:02 +0100

    Initial commit: Spring Boot setup

---
+++ src/main/java/com/gatekeeper/gatekeeper_web/GatekeeperWebApplication.java
@@ -0,0 +1,13 @@
+package com.gatekeeper.gatekeeper_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatekeeperWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatekeeperWebApplication.class, args);
+	}
+
+}
